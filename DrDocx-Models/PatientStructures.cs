ac0c3bf0c5926cb308c5da73abc602d5fb7760cc
Faiz <faiz.surani@gmail.com>
2020-02-11 11:14:00 -0800

    git subrepo clone https://github.com/DrDocx/DrDocx-Models DrDocx-Models
    subrepo:
      subdir:   "DrDocx-Models"
      merged:   "96953d2"
    upstream:
      origin:   "https://github.com/DrDocx/DrDocx-Models"
      branch:   "master"
      commit:   "96953d2"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

---
+++ DrDocx-Models/PatientStructures.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrDocxModels
+{
+
+    /// <summary>
+    /// Summary description for Patient
+    /// </summary>
+    ///
+    /// THIS WILL NOT WORK TIL EVERYTHINGS IN A PROJECT BECAUSE C# IS SPECIAL 
+    public class Patient
+    {
+        public List<TestResultGroup> ResultGroups { get; set; }
+        public string Diagnosis { get; set; }
+        public string Name { get; set; }
+        public string PreferredName { get; set; }
+        public string Address { get; set; }
+        public string Medications { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfTesting { get; set; }
+        public string Notes { get; set; }
+        public int MedicalRecordNumber { get; set; }
+        public int AgeAtTesting { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class TestResultGroup
+    {
+        public TestGroup TestGroupInfo { get; set; }
+        public List<TestResult> Tests { get; set; }
+
+        public int Id { get; set; }
+    }
+
+    public class TestResult
+    {
+        public int RawScore { get; set; }
+        public int ScaledScore { get; set; }
+        public int ZScore { get; set; }
+        public int Percentile { get; set; }
+        public Test RelatedTest { get; set; }
+
+        public int ID { get; set; }
+    }
+}
---
+++ DrDocx-Models/TestStructures.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DrDocxModels
+{
+    public class Test
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [JsonIgnore]
+        public List<TestGroupTest> TestGroupTests { get; set; }
+        public int Id { get; set; }
+
+        //public Test(string name, string description)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //}
+    }
+
+    public class TestGroup
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [JsonIgnore]
+        public List<TestGroupTest> TestGroupTests { get; set; }
+
+        public int Id { get; set; }
+
+        // This can be used for when a test group doesn't have tests ready to populate it with
+        //public TestGroup(string name, string description, int id)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //    Tests = null;
+        //    Id = id;
+        //}
+
+        //public TestGroup(string name, string description, List<DTest> tests, int id)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //    Tests = tests;
+        //    Id = id;
+        //}
+    }
+
+    public class TestGroupTest
+    {
+        public int Id { get; set; }
+        [JsonIgnore]
+        public Test Test { get; set; }
+        public int TestId { get; set; }
+        public TestGroup TestGroup { get; set; }
+        public int TestGroupId { get; set; }
+    }
+}
