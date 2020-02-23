b56dcedfc93accab3fedd562fbcc99cc9857e604
Faiz Surani <faiz.surani@gmail.com>
2020-02-23 14:03:36 -0800

    Split field models into separate files

---
+++ DrDocx-Models/FieldValue.cs
@@ -0,0 +1,11 @@
+﻿namespace DrDocx.Models
+{ 
+    public class FieldValue
+    {
+        public int Id { get; set; }
+        public Field Field { get; set; }
+        public FieldValueGroup FieldValueGroup { get; set; }
+        public string FieldTextValue { get; set; }
+    }
+
+}
---
+++ DrDocx-Models/FieldValueGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DrDocx.Models
+{
+    public class FieldValueGroup
+    {
+        public int Id { get; set; }
+        public FieldGroup FieldGroup { get; set; }
+        public Patient Patient { get; set; }
+        public List<FieldValue> FieldValues { get; set; }
+    }
+}
