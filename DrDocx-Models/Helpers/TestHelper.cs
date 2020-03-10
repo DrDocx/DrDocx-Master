d2921006e9e89904d8053b4f93ee36fbb2e7addc
Faiz Surani <faiz.surani@gmail.com>
2020-03-10 14:04:54 -0700

    Added z score and t score conversions to percentile

--- DrDocx-Models/Helpers/TestHelper.cs
+++ DrDocx-Models/Helpers/TestHelper.cs
@@ -7,10 +7,10 @@ namespace DrDocx.Models.Helpers
     {
         private static readonly string[] ValidScoreTypes =
         {
-            "SS (Wechsler)",
-            "Scaled Score (Wechsler)",
-            "T-Score",
-            "z-Score",
+            "SS",
+            "Scaled Score",
+            "T Score",
+            "z Score",
             "Percentile",
             "Other"
         };
@@ -22,23 +22,24 @@ namespace DrDocx.Models.Helpers
         {
             return scoreType switch
             {
-                "SS (Wechsler)" => GetPercentileForSS(score),
-                "Scaled Score (Wechsler)" => GetPercentileForScaledScore(score),
+                "SS" => GetPercentileForSS(score),
+                "Scaled Score" => GetPercentileForScaledScore(score),
                 "T Score" => GetPercentileForTScore(score),
                 "z Score" => GetPercentileForZScore(score),
                 "Percentile" => score,
-                _ => 0
+                _ => -1
             };
         }
 
         private static float GetPercentileForZScore(in float score)
         {
-            throw new NotImplementedException();
+            return GaussianCDF(score) * 100; // Convert from probability to percentile
         }
 
         private static float GetPercentileForTScore(in float score)
         {
-            throw new NotImplementedException();
+            var zScore = (score - 50) / 10;
+            return GetPercentileForZScore(zScore);
         }
 
         private static float GetPercentileForScaledScore(in float score)
@@ -49,6 +50,31 @@ namespace DrDocx.Models.Helpers
         private static float GetPercentileForSS(in float score)
         {
             throw new NotImplementedException();
+        }
+        
+        // Credit: https://www.johndcook.com/blog/csharp_phi/
+        private static float GaussianCDF(float zScore)
+        {
+            double x = zScore;
+            // constants
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+            double p = 0.3275911;
+        
+            // Save the sign of x
+            int sign = 1;
+            if (x < 0)
+                sign = -1;
+            x = Math.Abs(x) / Math.Sqrt(2.0);
+        
+            // A&S formula 7.1.26
+            double t = 1.0 / (1.0 + p*x);
+            double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t * Math.Exp(-x*x);
+        
+            return (float) (0.5 * (1.0 + sign*y));
         }
     }
 }
