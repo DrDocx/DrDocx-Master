83d3cf0970549210051cb0579e25078121f5de58
Faiz Surani <faiz.surani@gmail.com>
2020-05-09 17:29:15 -0700

    Added ping route for app startup

---
+++ DrDocx-API/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrDocx.API.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : Controller
+    {
+        // GET
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
