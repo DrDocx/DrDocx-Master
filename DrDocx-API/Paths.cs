93e7d093a1e8d161bab93ae5dd9eed80a76a1808
hari387 <narahari_rao@ucsb.edu>
2020-07-14 21:43:50 -0700

    Added directory for fieldGroup and subdirectories for import and export to Paths.cs

--- DrDocx-API/Paths.cs
+++ DrDocx-API/Paths.cs
@@ -14,6 +14,9 @@ namespace DrDocx.API
         public static string RelativeTemplatesDir => "templates";
         public static string RelativeTmpDir => "tmp";
         public static string RelativeReportsDir => "reports";
+		public static string RelativeFieldGroupsDir => "fieldGroups";
+		public static string RelativeFieldGroupsImportDir => Path.Combine(RelativeFieldGroupsDir,"import");
+		public static string RelativeFieldGroupsExportDir => Path.Combine(RelativeFieldGroupsDir,"export");
         public static string DbPath => Path.Combine(RelativeDbDir, "DrDocx.db");
 
         public static void EnsureDirsCreated()
@@ -25,6 +28,9 @@ namespace DrDocx.API
             CreateDirIfMissing(RelativeTemplatesDir);
             CreateDirIfMissing(RelativeTmpDir);
             CreateDirIfMissing(RelativeReportsDir);
+			CreateDirIfMissing(RelativeFieldGroupsDir);
+			CreateDirIfMissing(RelativeFieldGroupsExportDir);
+			CreateDirIfMissing(RelativeFieldGroupsImportDir);
         }
 
         private static void CreateDirIfMissing(string dirPath)
