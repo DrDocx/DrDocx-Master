e374e6f48e6f3422df20c387e5d9e41feb744c9d
Faiz Surani <faiz.surani@gmail.com>
2020-07-16 20:38:26 -0700

    fs - Add migration for test restructuring

--- DrDocx-API/Migrations/20200717030454_RestructureTests.Designer.cs
+++ DrDocx-API/Migrations/20200717030454_RestructureTests.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DrDocx.API;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DrDocx.API.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200717030454_RestructureTests")]
+    partial class RestructureTests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DrDocx-API/Migrations/20200717030454_RestructureTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrDocx.API.Migrations
+{
+    public partial class RestructureTests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestGroupTests");
+
+            migrationBuilder.RenameTable("Tests", null, "OldTests");
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    StandardizedScoreTypeId = table.Column<int>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    TestGroupId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tests_ScoreType_StandardizedScoreTypeId",
+                        column: x => x.StandardizedScoreTypeId,
+                        principalTable: "ScoreType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Tests_TestGroups_TestGroupId",
+                        column: x => x.TestGroupId,
+                        principalTable: "TestGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    
+                });
+            migrationBuilder.Sql(@"INSERT INTO Tests (Id, Name, StandardizedScoreTypeId, Description)
+                                   SELECT Id, Name, StandardizedScoreTypeId, Description
+                                   FROM OldTests
+                                ");
+
+            migrationBuilder.DropTable("OldTests");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tests_TestGroupId",
+                table: "Tests",
+                column: "TestGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
